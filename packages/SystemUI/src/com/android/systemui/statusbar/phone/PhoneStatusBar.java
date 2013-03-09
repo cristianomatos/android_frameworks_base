8eaa9516e01c102dd27b0bc2c6a93178f6f5323e
Lars Greiss <kufikugel@googlemail.com>
2013-03-09 22:04:01 +0100

    Frameworks: Add new configuration qualifier for UI change on the fly (1/3)
    last night I got an idea after 5 weeks of breaking stuff and
    doing to much complicated things. Together with mnazim we tried
    today to realize this idea. So another absolut new unique SlimBean
    feature is born.
    
    The aim we wanted at the end was to give a simple and resource friendly
    way to change the complete UI of a rom on the fly. So why not use
    the great fallback mechanism of AOSP and change/add a new
    qualifier into the AOSP core. To explain how it works.....if you want
    to add a new UI just add in /res folder of any app or framework you want
    folders with the qualifier "inverted". Eg. drawable-inverted-xhdpi
    or values-inverted or layout-port-inverted or whatever you want. Put in
    only the files you want to modify thats it.
    
    All qualifier in AOSP are following strict rules. You can read here.
    Do not forget the order of qualifier is important. The new "inverted"
    qualifier we added to the UIMode qualifier. To get more information
    about read here
    
    http://developer.android.com/guide/topics/resources/providing-resources.html
    
    So what this commit does:
    
    - add new qualifier "inverted"
    - support for API level 13 and higher
    - change the custom UI on the fly
    - control default UI Mode over config.xml
    - working for all apps. They just need to include folders
      with the new qualifier name
    - resource friendly....no resource wasting either programmticaly
      then source blow, because you just need to put in the files you
      want to change
    - cause of the qualifier structure compatible with every phone,
      language or whatever qualifier you want to use on top
    - 100% compatible to theme manager. If a theme misses drawables
      they are replaced with them from the current UIMode which is set.
    
    What is the benefit why not use just a theme? We started half year
    ago TRDS which was inverted gapps system base themed....means we
    morphed directly the changes into the framework. This methoed was
    on of the reasons why trds got so popular. Points for that are easy
    first the system is themed, means each theme you activate over
    theme engine works on top the themed system. So you have a base
    style and can modify more if you want. The other reason is easy
    a lot faster and resource friendly. Now this idea went to the
    rom to provide two core system based themes and user
    can do on top what they want or not without, like you have
    to roms in one.
    
    This commit just adds the support for this
    
    cheers
    
    mnazim and kufikugel
    
    Change-Id: I1ef843a42d17246373eb17b4e1ec1aa9bc20d8d4
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
    
    Conflicts:
    	api/13.xml
    	api/14.txt
    	api/15.txt
    	api/16.txt
    	api/17.txt
    	core/java/android/provider/Settings.java
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -232,6 +232,7 @@ public class PhoneStatusBar extends BaseStatusBar {
     // settings
     QuickSettingsController mQS;
     boolean mHasSettingsPanel, mHasFlipSettings;
+    boolean mUiModeIsToggled; 
     SettingsPanelView mSettingsPanel;
     View mFlipSettingsView;
     QuickSettingsContainerView mSettingsContainer;
@@ -588,6 +589,9 @@ public class PhoneStatusBar extends BaseStatusBar {
         mClearButton.setVisibility(View.GONE);
         mClearButton.setEnabled(false);
         mDateView = (DateView)mStatusBarWindow.findViewById(R.id.date);
+
+	mUiModeIsToggled = Settings.Secure.getInt(mContext.getContentResolver(),
+                              Settings.Secure.UI_MODE_IS_TOGGLED, 0) == 1; 
 
         mHasSettingsPanel = res.getBoolean(R.bool.config_hasSettingsPanel);
         mHasFlipSettings = res.getBoolean(R.bool.config_hasFlipSettingsPanel);
@@ -3262,10 +3266,14 @@ public class PhoneStatusBar extends BaseStatusBar {
         public void onChange(boolean selfChange) {
             onChange(selfChange, null);
 
+	boolean uiModeIsToggled = Settings.Secure.getInt(mContext.getContentResolver(),
+                                    Settings.Secure.UI_MODE_IS_TOGGLED, 0) == 1;
+
 	setNotificationWallpaperHelper();
             setNotificationAlphaHelper(); 
 
-        if (mSettingsContainer != null) {
+        if (mSettingsContainer != null
+	    || uiModeIsToggled != mUiModeIsToggled) { 
                 mQS.setupQuickSettings();
             } 
         }
@@ -3314,7 +3322,11 @@ public class PhoneStatusBar extends BaseStatusBar {
 	    cr.registerContentObserver(
                     Settings.System.getUriFor(Settings.System.NOTIF_WALLPAPER_ALPHA),
                     false, this, UserHandle.USER_ALL);
-                    setNotificationWallpaperHelper(); 
+                    setNotificationWallpaperHelper();
+
+	    cr.registerContentObserver(
+                    Settings.Secure.getUriFor(Settings.Secure.UI_MODE_IS_TOGGLED),
+                    false, this); 
 
             cr.registerContentObserver(
                     Settings.System.getUriFor(Settings.System.NOTIF_ALPHA),
