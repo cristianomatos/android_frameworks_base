8990df13b6aaf923613df9ca95184c5e4583db2e
beerbong <yghazi@gmail.com>
2013-07-17 08:03:46 +0200

    Kill floating activities running in the same thread when tapping outside This change makes all activities from the same app to be finished when we tap outside the halo window. Other apps running in different threads (floating or not) won't be finished.
    Change-Id: I3c51eafdcee7032fa90876411da3ff1eae0106c3

--- core/java/android/app/Activity.java
+++ core/java/android/app/Activity.java
@@ -4237,6 +4237,10 @@ public class Activity extends ContextThemeWrapper
         }
     }
 
+    public void finishFloating() {
+        mMainThread.performFinishFloating();
+    }
+
     /**
      * Finish this activity as well as all activities immediately below it
      * in the current task that have the same affinity.  This is typically
