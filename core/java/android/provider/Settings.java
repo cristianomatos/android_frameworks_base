8ce04d7ef3fa302035854a344008e51748b3952a
Adam Fisch <adammfisch@gmail.com>
2013-07-31 13:20:49 -0300

    Battery Bar + colorpicker (1/2) - battery bar collor - battery bar tickness - baterry bar style - battery bar animate
    Change-Id: I3984d28b801fc812988725321e38b6aacb5beb13

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2821,6 +2821,32 @@ public final class Settings {
          */
         public static final String STATUS_BAR_BATTERY = "status_bar_battery";
 
+	/**
+         * Whether to show the battery bar
+         * @hide
+         */
+        public static final String STATUSBAR_BATTERY_BAR = "statusbar_battery_bar";
+
+        /**
+         * @hide
+         */
+        public static final String STATUSBAR_BATTERY_BAR_COLOR = "statusbar_battery_bar_color";
+
+        /**
+         * @hide
+         */
+        public static final String STATUSBAR_BATTERY_BAR_THICKNESS = "statusbar_battery_bar_thickness";
+
+        /**
+         * @hide
+         */
+        public static final String STATUSBAR_BATTERY_BAR_STYLE = "statusbar_battery_bar_style";
+
+        /**
+         * @hide
+         */
+        public static final String STATUSBAR_BATTERY_BAR_ANIMATE = "statusbar_battery_bar_animate"; 
+
         /**
          * Whether to show the clock in status bar
          * of the stock battery icon
