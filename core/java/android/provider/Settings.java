ce14c345705ee901894e75d9b4b6318aab218e6f
benjamingoodwin <benjamin.j.goodwin@gmail.com>
2013-08-05 20:20:52 -0300

    Floating window (1/2) This is the setting part for the framework commit. This is a simple checkbox that allows the user to not make use of the floating window function seen in conjunction with PA's Halo feature. We are taking a different route on that Halo stuff and this is a very early start of it.
    Change-Id: I3d1e4bad7dc7ff61809ea91ff8fcd1dd671d9e06

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3001,6 +3001,12 @@ public final class Settings {
         public static final String HALO_PAUSE = "halo_pause";
 
 	/**
+         * Do you want popups/floating windows?
+         * @hide
+         */
+        public static final String WE_WANT_POPUPS = "we_want_popups"; 
+
+	/**
          * Sets transparency of statusbar
          * @hide
          */
