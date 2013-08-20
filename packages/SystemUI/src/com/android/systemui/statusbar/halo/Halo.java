5afd1c665addc1cfb434d66d8eb69078e9f2c4b9
drcmda <drcmda@gmail.com>
2013-08-20 17:19:12 +0100

    code cleanup
    Change-Id: I7af612c13aff48d0d20a0e8f83ea43c8941dcaaf

--- packages/SystemUI/src/com/android/systemui/statusbar/halo/Halo.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/halo/Halo.java
@@ -1301,7 +1301,6 @@ public class Halo extends FrameLayout implements Ticker.TickerCallback {
             mHaloBubble.draw(canvas);
             canvas.restoreToCount(state);
 
-
             // Number
             if (mState == State.IDLE || mState == State.GESTURES) {
                 state = canvas.save();
