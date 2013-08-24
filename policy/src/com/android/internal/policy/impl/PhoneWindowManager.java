aa60630d2e0c88bc7ad6909a35f986911332b5ed
Kiril Mikos <kiril.mik.os@gmail.com>
2013-08-24 21:37:57 +0900

    Fix rotation animation in expanded mode.
    When expanded mode is enabled we never changing
    value of mTopIsFullscreen but it should be updated so
    selectRotationAnimationLw will choose correct animation.
    
    Change-Id: I08906d4237696fe549b3799da38b58a0d56ac62c
    
    Conflicts:
    	policy/src/com/android/internal/policy/impl/PhoneWindowManager.java

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -3918,10 +3918,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             if (DEBUG_LAYOUT) Log.i(TAG, "force=" + mForceStatusBar
                     + " forcefkg=" + mForceStatusBarFromKeyguard
                     + " top=" + mTopFullscreenOpaqueWindowState);
-            if (expandedDesktopHidesStatusBar()) {
-                if (DEBUG_LAYOUT) Log.v(TAG, "Hiding status bar: expanded desktop enabled");
-                if (mStatusBar.hideLw(true)) changes |= FINISH_LAYOUT_REDO_LAYOUT;
-            } else if (mForceStatusBar || mForceStatusBarFromKeyguard) {
+            if (mForceStatusBar || mForceStatusBarFromKeyguard
+                    && !expandedDesktopHidesStatusBar()) { 
                 if (DEBUG_LAYOUT) Log.v(TAG, "Showing status bar: forced");
                 if (mStatusBar.showLw(true)) changes |= FINISH_LAYOUT_REDO_LAYOUT;
             } else if (mTopFullscreenOpaqueWindowState != null) {
@@ -3940,7 +3938,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 mHideStatusBar = Settings.System.getInt(mContext.getContentResolver(),
                         Settings.System.HIDE_STATUSBAR, 0) == 1;
                 if ((topIsFullscreen) || (mExpandedState == 1 && (mExpandedStyle == 2 
-            		|| mExpandedStyle == 3)) || (mHideStatusBar)) {   
+            		|| mExpandedStyle == 3)) || (mHideStatusBar) || expandedDesktopHidesStatusBar()) {   
                     if (DEBUG_LAYOUT) Log.v(TAG, "** HIDING status bar");
                     if (mStatusBar.hideLw(true)) {
                         changes |= FINISH_LAYOUT_REDO_LAYOUT;
