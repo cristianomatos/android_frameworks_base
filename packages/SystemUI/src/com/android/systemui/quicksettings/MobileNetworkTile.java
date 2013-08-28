21b39f20d505af3d02e87fba647296e5eab07dde
cristianomatos <cristianobmatos@gmail.com>
2013-08-28 07:04:22 -0300

    Flit tiles in MobileNetworkTile.
    Change-Id: Ib5b2d7446329be085a8b511f2415521e7ba73c01

--- packages/SystemUI/src/com/android/systemui/quicksettings/MobileNetworkTile.java
+++ packages/SystemUI/src/com/android/systemui/quicksettings/MobileNetworkTile.java
@@ -51,6 +51,9 @@ public class MobileNetworkTile extends QuickSettingsTile implements NetworkSigna
                     updateOverlayImage(DISABLED_OVERLAY);
                     mCm.setMobileDataEnabled(false);
                 }
+		if (isFlipTilesEnabled()) {
+                    flipTile(0);
+                }
             }
         };
 
