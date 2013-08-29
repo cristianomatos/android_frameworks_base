af9bd251d3288e300beff9379f161d94b9719e0e
Lars Greiss <kufikugel@googlemail.com>
2013-08-29 04:54:05 +0200

    Frameworks: add power menu to SlimActions (1/2)
    like the feature before we can see now how easy the util
    class and the dlsv is for us to use....just 8 lines + drawables
    and we have a new feature available on all navigations
    
    Change-Id: I88b5d5ded06294824679b0850725f14240c2ef92
    
    Conflicts:
    	core/java/com/android/internal/util/cm/ButtonsConstants.java
    	packages/SystemUI/src/com/android/systemui/SearchPanelView.java

--- packages/SystemUI/src/com/android/systemui/SearchPanelView.java
+++ packages/SystemUI/src/com/android/systemui/SearchPanelView.java
@@ -342,6 +342,8 @@ public class SearchPanelView extends FrameLayout implements
             return new TargetDrawable(mResources, mResources.getDrawable(R.drawable.ic_action_lastapp));
         if (action.equals(ButtonsConstants.ACTION_POWER))
             return new TargetDrawable(mResources, mResources.getDrawable(R.drawable.ic_action_power));
+	if (action.equals(ButtonsConstants.ACTION_POWER_MENU))
+            return new TargetDrawable(mResources, mResources.getDrawable(R.drawable.ic_action_power_menu));
 	if (action.equals(ButtonsConstants.ACTION_NOTIFICATIONS))
             return new TargetDrawable(mResources, mResources.getDrawable(R.drawable.ic_action_notifications));
         if (action.equals(ButtonsConstants.ACTION_EXPANDED_DESKTOP))
