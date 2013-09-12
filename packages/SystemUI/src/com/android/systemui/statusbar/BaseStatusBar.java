eac022411090b4b0fd1f646841355f1b34f4f8cc
ayysir <dresadd09691@gmail.com>
2013-09-12 12:49:00 -0400

    Halo Mod fixup Conflicts: 	packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
    Change-Id: I3df5943f8fc17e1219290bc0e12d29706308d227

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -599,7 +599,7 @@ public abstract class BaseStatusBar extends SystemUI implements
         updateHalo();
     }
 
-    protected void updateHalo() {
+    public void updateHalo() {
         mHaloEnabled = Settings.System.getInt(mContext.getContentResolver(),
                 Settings.System.HALO_ENABLED, 0) == 1;
         mHaloActive = Settings.System.getInt(mContext.getContentResolver(),
