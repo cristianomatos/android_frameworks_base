e2498c7211a9cf1962052da89abc24f55d7ba0ff
Schubi <schubi@erlangen.ccc.de>
2013-09-26 15:32:19 +0200

    Active display: Fix "turn off display if device gets pocketed again"
    Change-Id: Ifc2e148fca44c1ff143e93283c4c75319cf79bf9

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/activedisplay/ActiveDisplayView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/activedisplay/ActiveDisplayView.java
@@ -662,6 +662,7 @@ public class ActiveDisplayView extends FrameLayout {
     }
 
     private void onScreenTurnedOff() {
+        mWakedByPocketMode = false;
         hideNotificationView();
         cancelTimeoutTimer();
         if (mRedisplayTimeout > 0) updateRedisplayTimer();
