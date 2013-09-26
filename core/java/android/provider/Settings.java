5cc2d0b7e4a5d763c8e562dad4ecba31955b0b1c
xplodwild <xplod@ouverta.fr>
2013-09-26 14:39:39 +0100

    [1/2] Add battery level around unlock ring
    Change-Id: Ia4dbc2335657c096c34349ec67c4adfec0aeb91b
    
    Conflicts:
    	core/java/com/android/internal/widget/multiwaveview/GlowPadView.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4274,6 +4274,12 @@ public final class Settings {
         public static final String SYSTEMUI_NAVRING_CONFIG = "systemui_navring_config"; 
 
         /**
+         * Whether or not to show circle battery around the lockscreen ring
+         * @hide
+         */
+        public static final String BATTERY_AROUND_LOCKSCREEN_RING = "battery_around_lockscreen_ring";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
