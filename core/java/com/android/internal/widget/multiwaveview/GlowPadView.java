1e2456588f8906bc6e4885aabfa484f8128843a8
cristianomatos <cristianobmatos@gmail.com>
2013-10-04 15:17:15 -0300

    Revert "Active display: Make GlowPadView center the current notification icon for us"
    This reverts commit 05ed21de961bf565eaf7834a9fffbe8b67435867.

--- core/java/com/android/internal/widget/multiwaveview/GlowPadView.java
+++ core/java/com/android/internal/widget/multiwaveview/GlowPadView.java
@@ -147,8 +147,6 @@ public class GlowPadView extends View {
     private TargetDrawable mOuterRing;
     private Vibrator mVibrator;
 
-    private TargetDrawable mCenterDrawable; 
-
     private int mFeedbackCount = 3;
     private int mVibrationDuration = 0;
     private int mGrabbedState;
@@ -283,8 +281,6 @@ public class GlowPadView extends View {
         TypedValue handle = a.peekValue(R.styleable.GlowPadView_handleDrawable);
         mHandleDrawable = new TargetDrawable(res, handle != null ? handle.resourceId : 0);
         mHandleDrawable.setState(TargetDrawable.STATE_INACTIVE);
-	mCenterDrawable = new TargetDrawable(res, 0);
-        mCenterDrawable.setState(TargetDrawable.STATE_INACTIVE); 
         mOuterRing = new TargetDrawable(res,
                 getResourceId(a, R.styleable.GlowPadView_outerRingDrawable));
 
@@ -434,8 +430,6 @@ public class GlowPadView extends View {
                 startBackgroundAnimation(0, 0.0f);
                 mHandleDrawable.setState(TargetDrawable.STATE_INACTIVE);
                 mHandleDrawable.setAlpha(1.0f);
-		mCenterDrawable.setState(TargetDrawable.STATE_INACTIVE);
-                mCenterDrawable.setAlpha(1.0f); 
 		mPaintText.setAlpha(255);
                 break;
 
@@ -446,7 +440,6 @@ public class GlowPadView extends View {
             case STATE_FIRST_TOUCH:
 		mPaintText.setAlpha(0); 
                 mHandleDrawable.setAlpha(0.0f);
-		mCenterDrawable.setAlpha(0.0f); 
                 deactivateTargets();
                 showTargets(true);
                 startBackgroundAnimation(INITIAL_SHOW_HANDLE_DURATION, 1.0f);
@@ -459,7 +452,6 @@ public class GlowPadView extends View {
             case STATE_TRACKING:
 		mPaintText.setAlpha(0); 
                 mHandleDrawable.setAlpha(0.0f);
-		mCenterDrawable.setAlpha(0.0f); 
                 showGlow(REVEAL_GLOW_DURATION , REVEAL_GLOW_DELAY, 1.0f, null);
                 break;
 
@@ -467,7 +459,6 @@ public class GlowPadView extends View {
                 // TODO: Add transition states (see list_selector_background_transition.xml)
 		mPaintText.setAlpha(0); 
                 mHandleDrawable.setAlpha(0.0f);
-		mCenterDrawable.setAlpha(0.0f); 
                 showGlow(REVEAL_GLOW_DURATION , REVEAL_GLOW_DELAY, 0.0f, null);
                 break;
 
@@ -1240,8 +1231,6 @@ public class GlowPadView extends View {
 
         mHandleDrawable.setPositionX(newWaveCenterX);
         mHandleDrawable.setPositionY(newWaveCenterY);
-	mCenterDrawable.setPositionX(newWaveCenterX);
-        mCenterDrawable.setPositionY(newWaveCenterY); 
 
         updateTargetPositions(newWaveCenterX, newWaveCenterY);
         updatePointCloudPosition(newWaveCenterX, newWaveCenterY);
@@ -1309,7 +1298,6 @@ public class GlowPadView extends View {
                 target.draw(canvas);
             }
         }
-	mCenterDrawable.draw(canvas); 
         mHandleDrawable.draw(canvas);
 
 	if (!TextUtils.isEmpty(mHandleText) && mPaintText.getAlpha() != 0) {
@@ -1335,12 +1323,6 @@ public class GlowPadView extends View {
                 text, new TextPaint(mPaintText), mMaxTextArcLength,
                 TextUtils.TruncateAt.END).toString();
     }
-
-    public void setCenterDrawable(TargetDrawable d) {
-        mCenterDrawable = new TargetDrawable(d);
-        mCenterDrawable.setPositionX(mWaveCenterX);
-        mCenterDrawable.setPositionY(mWaveCenterY);
-    } 	
 
     public void setDrawOuterRing(boolean drawOuterRing) {
         mDrawOuterRing = drawOuterRing; 
