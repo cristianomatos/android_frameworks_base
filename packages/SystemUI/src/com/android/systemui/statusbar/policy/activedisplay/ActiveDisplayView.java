562e9e44e6de7c23a1830bd6474efff99c244ed3
Der-Schubi <schubi@erlangen.ccc.de>
2013-10-09 10:42:26 -0300

    Active display: Fix visible NavBar buttons on Lockscreen
    [log]
    
    Change-Id: Iae98ada2483fd6fc35f33178b61791356ec8d0a2

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/activedisplay/ActiveDisplayView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/activedisplay/ActiveDisplayView.java
@@ -627,7 +627,7 @@ public class ActiveDisplayView extends FrameLayout {
         setVisibility(View.GONE);
         restoreBrightness();
 	mWakedByPocketMode = false; 
-        mBar.disable(0);
+        //mBar.disable(0);
         cancelTimeoutTimer();
         unregisterSensorListener(mLightSensor); 
     }
