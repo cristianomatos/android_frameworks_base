845f2940e3c4a03999318b49417cfc1ac78ffe28
Lars Greiss <kufikugel@googlemail.com>
2013-10-10 02:12:54 +0200

    Frameworks: Privacy Guard part 2 (1/2)
    Current implementation does not really merge both awesome features
    appops and privacy guard. As well it is from UX perspective too complicated
    
    So this commits changes:
    
    - Move int array to logical correct place AppOpsManager
    - Switch only the privacy guard permissions which the app
      really requests and not all...this irritates the user
    - give support for custom permission set and reflect this state
      with an icon in notification and pgm
    - add pack filterpermission feature due that user mainly are interested
      in the apps which use critical personal data
    - permit by default as well writing off personal data
    - do not show apps where either permissions are not changeable due
      off appops or apps which do not have permissions at all
    - ability to hide pg notification
    - remove pg checkbox in InstalledAppdetails and add a button to
      modify appops. Button is disabled if either the app does not have
      permissions or appops does not support it. As well for platform
      certificate apps
    - add on appops detail screen reset and privacy guard button to quick
      change all states
    - rework of the complete dialog handling in pgm and add several dialogs
    - move all notify icon for pgm and notification to frameworks base
    - and a lot more minor fixes and fc fixes
    
    cheers
    
    PatchSet:
    - correct privacy guard filter logic
    - add pgm map
    - add status icons.....PGM shows either off/partly enabled/enabled
      in addition to it other set permissions shows a red plus sign on
      the top right corner of the PGM icon on all states
    - tapping on PGM and menu pgm button on detail appops screen turns on/off
      only PGM permissions to be more consistent
    - some minor fixes
    
    Change-Id: I990a6c7f25cdee5febdb95c1e26c022d6bcb27e0
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	core/res/res/values/cm_strings.xml
    	core/res/res/values/slim_symbols.xml

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -471,6 +471,19 @@ public final class Settings {
 
     /**
      * @hide
+     * Activity Action: Show the "app ops" details screen.
+     * <p>
+     * Input: The Intent's data URI specifies the application package name
+     * to be shown, with the "package" scheme.  That is "package:com.my.app".
+     * <p>
+     * Output: Nothing.
+     */
+    @SdkConstant(SdkConstantType.ACTIVITY_INTENT_ACTION)
+    public static final String ACTION_APP_OPS_DETAILS_SETTINGS =
+            "android.settings.APP_OPS_DETAILS_SETTINGS"; 	
+
+    /**
+     * @hide
      * Activity Action: Show the "app ops" settings screen.
      * <p>
      * Input: Nothing.
@@ -6161,6 +6174,12 @@ public final class Settings {
          * @hide
          */
         public static final String SCREENSAVER_DEFAULT_COMPONENT = "screensaver_default_component";
+
+	/**
+         * Whether privacy guard notification should show.
+         * @hide
+         */
+         public static final String PRIVACY_GUARD_NOTIFICATION = "privacy_guard_notification";
 
         /**
          * Name of a package that the current user has explicitly allowed to see all of that
