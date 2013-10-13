e0fc102473bd0f20e0f782f18e516750cd6c0c89
cristianomatos <cristianobmatos@gmail.com>
2013-10-13 10:59:52 -0300

    Navigation ring custom shortucts (1/2)
    Change-Id: If87f570e995b6b81f23c5502cae091c84da3c9e4

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3176,7 +3176,18 @@ public final class Settings {
          */
         public static final String CUSTOM_CARRIER_LABEL = "custom_carrier_label";  
 
-	/**
+        /**
+         * Custom navring actions
+         *
+         * @hide
+         */
+        public static final String[] NAVIGATION_RING_TARGETS = new String[] {
+            "navigation_ring_targets_0",
+            "navigation_ring_targets_1",
+            "navigation_ring_targets_2",
+        };
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
