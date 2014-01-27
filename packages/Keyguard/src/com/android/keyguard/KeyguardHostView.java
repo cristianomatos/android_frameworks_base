def4cc753cb36ee3407885c23f046087cf4cf931
0xD34D <clark@scheffsblend.com>
2014-01-27 00:09:43 -0600

    Gesture Lockscreen [1/2]
    Change-Id: I9820644d0a5752d62b8219e820e696c9157e949d

--- packages/Keyguard/src/com/android/keyguard/KeyguardHostView.java
+++ packages/Keyguard/src/com/android/keyguard/KeyguardHostView.java
@@ -679,6 +679,9 @@ public class KeyguardHostView extends KeyguardViewBase {
             case Password:
                 messageId = R.string.kg_too_many_failed_password_attempts_dialog_message;
                 break;
+            case Gesture:
+                messageId = R.string.kg_too_many_failed_gesture_attempts_dialog_message;
+                break;
         }
 
         if (messageId != 0) {
@@ -831,6 +834,7 @@ public class KeyguardHostView extends KeyguardViewBase {
                 case PIN:
                 case Account:
                 case Biometric:
+                case Gesture:
                     finish = true;
                     break;
 
@@ -1189,6 +1193,7 @@ public class KeyguardHostView extends KeyguardViewBase {
                     return R.id.msim_keyguard_sim_puk_view;
                 }
                 return R.id.keyguard_sim_puk_view;
+            case Gesture: return R.id.keyguard_gesture_view;
         }
         return 0;
     }
@@ -1211,6 +1216,7 @@ public class KeyguardHostView extends KeyguardViewBase {
                     return R.layout.msim_keyguard_sim_puk_view;
                 }
                 return R.layout.keyguard_sim_puk_view;
+            case Gesture: return R.layout.keyguard_gesture_view;
             default:
                 return 0;
         }
