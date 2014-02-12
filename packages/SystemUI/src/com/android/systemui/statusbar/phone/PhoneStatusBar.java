16305a3bd0b51063bde1563d34dab6d66561772f
Lars Greiss <kufikugel@googlemail.com>
2014-02-12 15:37:19 -0200

    fb: TRDS some tweaks and fixes (1/2)
    some smaller issues fixed and optimizations:
    
    - fix twilightmode was wrong assigned to wrong value
    - ligtsensor mode after first immediattely switch it does
      not allow for 5sec to switch again....after the 5sec it switches
      to current state if the sensor changed its state in this time.
      This prevends on the one hand on bad light conditions a permanent
      switch between dark/light as well it does not bring the system into
      troubles due of to much configuration changes at the same time
    - get rid completly of the observe for the UI_THEME_MODE value and
      handle all over the UIModeManager
    - adapt slimActions and settings to this new behaviour/method
    - some cleanup and minor fixes
    
    Change-Id: I4b2401bf7bef992ad3112ae5ccc852cd29b46bd0
    
    Conflicts:
    	core/java/com/android/internal/util/slim/SlimActions.java
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -3315,33 +3315,25 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
 
         // detect theme change.
         CustomTheme newTheme = res.getConfiguration().customTheme;
+        int uiThemeMode = res.getConfiguration().uiThemeMode;
         if (newTheme != null &&
                 (mCurrentTheme == null || !mCurrentTheme.equals(newTheme))) {
             mCurrentTheme = (CustomTheme)newTheme.clone();
             recreateStatusBar();
-        } else {
 
-            if (mClearButton instanceof TextView) {
+        if (uiThemeMode != mCurrUiThemeMode) {
+            mCurrUiThemeMode = uiThemeMode;
+            //recreateStatusBar();
+        }
+
+        if (mClearButton instanceof TextView) {
                 ((TextView)mClearButton).setText(context.getText(R.string.status_bar_clear_all_button));
             }
             loadDimens();
         }
 
-        // detect theme ui mode change
-        int uiThemeMode = res.getConfiguration().uiThemeMode;
-        if (uiThemeMode != mCurrUiThemeMode) {
-            mCurrUiThemeMode = uiThemeMode;
-            //recreateStatusBar();
-            return;
-        }
-
-        if (mClearButton instanceof TextView) {
-            ((TextView)mClearButton).setText(context.getText(R.string.status_bar_clear_all_button));
-        }
-
         // Update the QuickSettings container
         if (mQS != null) mQS.updateResources();
-        
     }
 
     protected void loadDimens() {
