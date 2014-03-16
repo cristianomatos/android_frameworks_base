ee33fd168a70706d5bebcc48c57d4c39183c7109
cristianomatos <cristianobmatos@gmail.com>
2014-03-16 08:19:47 -0300

    Revert "QS: let's welcome LocationTile to 4.4"
    This reverts commit eb959849f97ca273274bf7f6cd1914f2acac28eb.

--- packages/SystemUI/src/com/android/systemui/quicksettings/GPSTile.java
+++ packages/SystemUI/src/com/android/systemui/quicksettings/GPSTile.java
@@ -1,26 +1,11 @@
-/*
- * Copyright (C) 2012 The Android Open Source Project
- * Copyright (C) 2013 The SlimRoms Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.android.systemui.quicksettings;
 
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.location.LocationManager;
 import android.provider.Settings;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.View.OnLongClickListener;
@@ -31,21 +16,19 @@ import com.android.systemui.statusbar.phone.QuickSettingsContainerView;
 import com.android.systemui.statusbar.policy.LocationController;
 import com.android.systemui.statusbar.policy.LocationController.LocationSettingsChangeCallback;
 
+
 public class GPSTile extends QuickSettingsTile implements LocationSettingsChangeCallback {
 
-    private QuickSettingsController mQsc;
+    ContentResolver mContentResolver;
     private LocationController mLocationController;
     private boolean mLocationEnabled;
-    private int mLocationMode;
 
-    public GPSTile(Context context, final QuickSettingsController qsc) {
+    public GPSTile(Context context, QuickSettingsController qsc) {
         super(context, qsc);
 
-        mQsc = qsc;
+        mContentResolver = mContext.getContentResolver();
         mLocationController = new LocationController(mContext);
         mLocationController.addSettingsChangedCallback(this);
-        mLocationMode = mLocationController.getLocationMode();
-        mLocationEnabled = mLocationController.isLocationEnabled();
 
         mOnClick = new OnClickListener() {
             @Override
@@ -57,9 +40,7 @@ public class GPSTile extends QuickSettingsTile implements LocationSettingsChange
         mOnLongClick = new OnLongClickListener() {
             @Override
             public boolean onLongClick(View v) {
-                if (mLocationEnabled) {
-                    mLocationController.switchLocationMode(mLocationMode);
-                }
+                startSettingsActivity(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
                 return true;
             }
         };
@@ -74,40 +55,19 @@ public class GPSTile extends QuickSettingsTile implements LocationSettingsChange
     @Override
     public void updateResources() {
         updateTile();
-        super.updateResources();
+        updateQuickSettings();
     }
 
     private synchronized void updateTile() {
-        switch (mLocationMode) {
-            case Settings.Secure.LOCATION_MODE_SENSORS_ONLY:
-                mDrawable = R.drawable.ic_qs_location_on_gps;
-                break;
-            case Settings.Secure.LOCATION_MODE_BATTERY_SAVING:
-                mDrawable = R.drawable.ic_qs_location_on_wifi;
-                break;
-            case Settings.Secure.LOCATION_MODE_HIGH_ACCURACY:
-                mDrawable = R.drawable.ic_qs_location_on;
-                break;
-            case Settings.Secure.LOCATION_MODE_OFF:
-                mDrawable = R.drawable.ic_qs_location_off;
-                break;
-        }
         int textResId = mLocationEnabled ? R.string.quick_settings_location_label
                 : R.string.quick_settings_location_off_label;
         mLabel = mContext.getText(textResId).toString();
+        mDrawable = mLocationEnabled
+                ? R.drawable.ic_qs_location_on : R.drawable.ic_qs_location_off;
     }
 
     @Override
-    public void onLocationSettingsChanged(boolean locationEnabled, int locationMode) {
-        // collapse all panels in case the confirmation dialog needs to show up
-        if ((mLocationMode == Settings.Secure.LOCATION_MODE_SENSORS_ONLY
-                        && locationMode == Settings.Secure.LOCATION_MODE_HIGH_ACCURACY)
-                || (!mLocationEnabled && locationEnabled
-                        && (locationMode == Settings.Secure.LOCATION_MODE_HIGH_ACCURACY
-                        || locationMode == Settings.Secure.LOCATION_MODE_BATTERY_SAVING))) {
-            mQsc.mBar.collapseAllPanels(true);
-        }
-        mLocationMode = locationMode;
+    public void onLocationSettingsChanged(boolean locationEnabled) {
         mLocationEnabled = locationEnabled;
         updateResources();
     }
