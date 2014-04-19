834267d500700ee2cd2293fdd4f6f3881830b4a0
David96 <xd_e-mail@gmx.de>
2014-04-19 20:35:57 -0300

    [SQUASHED] Lockscreen Notifications (1/2)
    Following commits inside
    AOSPAL/android_frameworks_base@3487cda
    AOSPAL/android_frameworks_base@5873e99
    AOSPAL/android_frameworks_base@7350e91
    AOSPAL/android_frameworks_base@8d99eed
    AOSPAL/android_frameworks_base@181110c
    AOSPAL/android_frameworks_base@ce14cdd
    AOSPAL/android_frameworks_base@3914e8d
    AOSPAL/android_frameworks_base@32ec14f
    AOSPAL/android_frameworks_base@6d2f41d
    AOSPAL/android_frameworks_base@b758e16
    AOSPAL/android_frameworks_base@6ba6fdb
    AOSPAL/android_frameworks_base@b20bdc9
    AOSPAL/android_frameworks_base@25f75c6
    AOSPAL/android_frameworks_base@a74309e
    AOSPAL/android_frameworks_base@5b6e143
    AOSPAL/android_frameworks_base@e9dcb99
    AOSPAL/android_frameworks_base@4caa49c
    AOSPAL/android_frameworks_base@0f2377c
    AOSPAL/android_frameworks_base@7e75b49
    AOSPAL/android_frameworks_base@bc6c83f Thanks @Der-Schubi
    AOSPAL/android_frameworks_base@8fd230d
    AOSPAL/android_frameworks_base@e05eba6
    AOSPAL/android_frameworks_base@082774a (Partialy ported - Removed codes of clear notifications navbar button)
    AOSPAL/android_frameworks_base@a6f92e5
    AOSPAL/android_frameworks_base@c3d76f5
    AOSPAL/android_frameworks_base@5b1d884
    AOSPAL/android_frameworks_base@0c0b133
    AOSPAL/android_frameworks_base@73a6b7d
    
    Change-Id: Ib87fe4d11941f7879f4706ad27ff3408311d2c03

--- services/java/com/android/server/NotificationManagerService.java
+++ services/java/com/android/server/NotificationManagerService.java
@@ -793,9 +793,10 @@ public class NotificationManagerService extends INotificationManager.Stub
         for (NotificationListenerInfo info : toRemove) {
             final ComponentName component = info.component;
             final int oldUser = info.userid;
-            Slog.v(TAG, "disabling notification listener for user " + oldUser + ": " + component);
-            // Do not un-register HALO, we un-register only when HALO is closed
-            if (!component.getPackageName().equals("HaloComponent")) unregisterListenerService(component, info.userid);
+            if (!info.isSystem && !component.getPackageName().equals("HaloComponent")) {
+                Slog.v(TAG, "disabling notification listener for user " + oldUser + ": " + component);
+                unregisterListenerService(component, info.userid);
+            }
         }
 
         final int N = toAdd.size();
@@ -816,7 +817,11 @@ public class NotificationManagerService extends INotificationManager.Stub
     @Override
     public void registerListener(final INotificationListener listener,
             final ComponentName component, final int userid) {
-	if (!component.getPackageName().equals("HaloComponent")) checkCallerIsSystem();        
+        final int permission = mContext.checkCallingPermission(
+                android.Manifest.permission.SYSTEM_NOTIFICATION_LISTENER);
+        if (permission == PackageManager.PERMISSION_DENIED && !component.getPackageName().equals("HaloComponent")) {
+            checkCallerIsSystem();
+        }
 
         synchronized (mNotificationList) {
             try {
