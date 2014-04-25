6869a001e2228a21149b0b65009165ad850d7b6a
Roman Birg <roman@cyngn.com>
2014-04-24 22:22:45 -0700

    frameworks: move keyguard wallpaper to system  (1/2)
    Change-Id: I0eb1b5578b705c2ade4c0772fa86e09478b9f73e
    Signed-off-by: Roman Birg <roman@cyngn.com>
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -54,7 +54,6 @@ import android.text.TextUtils;
 import android.util.AndroidException;
 import android.util.Log;
 
-import com.android.internal.util.cm.LockscreenBackgroundUtil;
 import com.android.internal.widget.ILockSettings;
 
 import java.net.URISyntaxException;
