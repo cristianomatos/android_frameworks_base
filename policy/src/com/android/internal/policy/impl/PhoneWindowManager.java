e7d4aedc3d3fa2dd2d7231b0979ae0bc7ddafc7e
Cristiano Matos <cristianobmatos@gmail.com>
2014-05-12 06:47:17 -0300

    Kill app long press timeout: fix indiscriminately killing in clean instalations (1/2)
    Change-Id: I9ccfd3ba4d27d2b2175ee0134223308694653b8a

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -742,6 +742,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.NAVIGATION_BAR_WIDTH), false, this,
                     UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.Secure.getUriFor(
+                    Settings.Secure.KILL_APP_LONGPRESS_TIMEOUT), false, this,
+                    UserHandle.USER_ALL);
 
             updateSettings();
         }
@@ -2888,7 +2891,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                     Settings.Secure.KILL_APP_LONGPRESS_BACK, 0, UserHandle.USER_CURRENT) == 1) {
                 if (down && repeatCount == 0) {
                     mBackKillTimeout = Settings.Secure.getIntForUser(mContext.getContentResolver(),
-                        Settings.Secure.KILL_APP_LONGPRESS_TIMEOUT, 0, UserHandle.USER_CURRENT);
+                        Settings.Secure.KILL_APP_LONGPRESS_TIMEOUT, 2000, UserHandle.USER_CURRENT);
                     mHandler.postDelayed(mBackLongPress, mBackKillTimeout);
                 }
             }
