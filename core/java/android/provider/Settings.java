4d178f1edca7911b81c76e89ca51480170800e5c
dankoman <dankoman30@gmail.com>
2014-08-22 23:38:46 +0200

    FB: Slimrecents card/text color [2/2]
    Allows user to set custom colors for Slimrecents card
        header text and background.
    
    PS: fixed expanded card (thumbnail) background
    Change-Id: I896a4db4ce235dc53858381347379d5fd7f7e796
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4648,6 +4648,20 @@ public final class Settings {
         public static final int INCOMING_CALL_STYLE_FULLSCREEN_PHOTO = 1;
 
         /**
+         * Recent card background color
+         *
+         * @hide
+         */
+        public static final String RECENT_CARD_BG_COLOR = "recent_card_bg_color";
+
+        /**
+         * Recent card text color
+         *
+         * @hide
+         */
+        public static final String RECENT_CARD_TEXT_COLOR = "recent_card_text_color";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
