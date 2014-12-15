7900a62bb87734a299b10bb3d0030b888c25609b
Lars Greiss <kufikugel@googlemail.com>
2014-12-15 15:16:42 +0100

    Frameworks: add ability to change the color in battery saver mode (1/2)
    User complain about the ugly orange....give the user the ability to change the
    color in status and navigation bar if battery saver mode is activated.
    
    Especially for you Tylog :D
    
    PS
    - move it into the observer...method is called to often to call every time
      the settings value for it.
    
    Change-Id: I48ed2e85c462a2e94770b2bc1ad75b1ca40f2304
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2121,6 +2121,13 @@ public final class Settings {
         public static final String HEADS_UP_NOTIFCATION_DECAY = "heads_up_notification_decay";
 
         /**
+         * Defines the status and navigation bar color is battery safe mode is enabled.
+         *
+         * @hide
+         */
+        public static final String BATTERY_SAVER_MODE_COLOR = "battery_save_mode_color";
+
+        /**
          * Control whether the process CPU usage meter should be shown.
          *
          * @deprecated Use {@link Global#SHOW_PROCESSES} instead
