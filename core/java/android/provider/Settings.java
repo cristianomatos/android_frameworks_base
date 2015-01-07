1f7e75b12775d1567bad0c1f9e579ccaa25e23fe
TheMrcool212 <themrcool212.android@gmail.com>
2015-01-07 13:35:47 +0000

    SystemUI: option to ignore the last app when clearing recents (1/2)
    Change-Id: I40e4bbd5c06f367e3282b2f8ac614f1f0305c74b

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3394,6 +3394,12 @@ public final class Settings {
         public static final String RECENTS_SHOW_HIDE_SEARCH_BAR = "recents_show_hide_search_bar";
 
         /**
+         * Whether to dismiss all recents tasks
+         * @hide
+         */
+        public static final String RECENTS_CLEAR_ALL_DISMISS_ALL = "recents_clear_all_dismiss_all";
+
+        /**
           * Volume keys control cursor in text fields (default is 0)
           * 0 - Disabled
           * 1 - Volume up/down moves cursor left/right
